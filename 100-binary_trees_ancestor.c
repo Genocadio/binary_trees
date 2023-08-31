50f0ce76ae7b3bd259a03277a0b619639c4a8a8a
geno <yvesgeno@outlook.com>
2023-09-01 00:42:07 +0200

    correction

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,21 +6,22 @@
  *
  * Return: pointer to the lowest common ancestor
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
 {
-		binary_tree_t *array[256];
-		int i, j;
+	binary_tree_t *array[256];
+	int i, j;
 
-		if (!first || !second)
-			return (NULL);
-		for (i = 0; first; first = first->parent, i++)
-			array[i] = (binary_tree_t *)first;
-		while (second)
-		{
-			for (j = 0; j < i; j++)
-				if (second == array[j])
-					return (array[j]);
-			second = second->parent;
-		}
+	if (!first || !second)
 		return (NULL);
+	for (i = 0; first; first = first->parent, i++)
+		array[i] = (binary_tree_t *)first;
+	while (second)
+	{
+		for (j = 0; j < i; j++)
+			if (second == array[j])
+				return (array[j]);
+		second = second->parent;
+	}
+	return (NULL);
 }
